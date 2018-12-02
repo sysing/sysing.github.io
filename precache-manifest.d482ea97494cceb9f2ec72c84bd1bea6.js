73cce69112cb17fd97053761fa4dcc388b72323e
sweeyang <sweeyangsing@gmail.com>
2018-12-02 19:26:49 +0800

    Updates

---
+++ precache-manifest.d482ea97494cceb9f2ec72c84bd1bea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea3b78c2096bf3568ae4",
+    "url": "/static/js/main.ea3b78c2.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "ea3b78c2096bf3568ae4",
+    "url": "/static/css/main.1e6f26f3.chunk.css"
+  },
+  {
+    "revision": "50977904383333b3b0d1b613f354e3f2",
+    "url": "/index.html"
+  }
+];
